f889d12627db351c0c41b1d08006ebe2db188fb0
thirilinn-coder <thirilinn.developer@gmail.com>
2026-09-02 23:31:49 +0630

    feat: student crud using dapper

---
+++ DotNetPractice.ConsoleApp5/DapperService.cs
@@ -0,0 +1,91 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetPractice.ConsoleApp5
+{
+    internal class DapperService
+    {
+        private readonly SqlConnectionStringBuilder sb = new SqlConnectionStringBuilder()
+        {
+            DataSource = ".",
+            InitialCatalog = "DotNetPractice",
+            UserID = "sa",
+            Password = "sasa@123",
+            TrustServerCertificate = true
+        };
+
+        public void Read()
+        {
+            using (IDbConnection db = new SqlConnection(sb.ConnectionString))
+            {
+                db.Open();
+
+                List<dynamic> lst = db.Query<dynamic>(@"SELECT [StudentId]
+      ,[StudentName]
+      ,[Email]
+      ,[Password]
+  FROM [dbo].[Tbl_Student]").ToList();
+
+                foreach (var item in lst)
+                {
+                    Console.WriteLine(item.StudentId);
+                    Console.WriteLine(item.StudentName);
+                    Console.WriteLine(item.Email);
+                    Console.WriteLine(item.Password);
+                }
+            }
+        }
+
+        public void Create()
+        {
+            using (IDbConnection db = new SqlConnection(sb.ConnectionString))
+            {
+                db.Open();
+                int result = db.Execute(@"INSERT INTO [dbo].[Tbl_Student]
+           ([StudentName]
+           ,[Email]
+           ,[Password])
+VALUES
+           ('Linn', 'linn@gmail.com', '12345'),
+           ('Aung Aung', 'aung@gmail.com', '67890'),
+           ('Su Su', 'susu@gmail.com', 'abc123'); ");
+
+                Console.WriteLine("Row affected: " + result);
+            }
+        }
+
+        public void Update()
+        {
+            using (IDbConnection db = new SqlConnection(sb.ConnectionString))
+            {
+                db.Open();
+                int result = db.Execute(@"UPDATE [dbo].[Tbl_Student]
+SET [StudentName] = 'Piti',
+    [Email] = 'piti@gmail.com',
+    [Password] = 'piti123'
+WHERE [StudentName] = 'Su Su';");
+
+                Console.WriteLine("Row affected: " + result);
+            }
+        }
+
+        public void Delete()
+        {
+            using (IDbConnection db = new SqlConnection(sb.ConnectionString))
+            {
+                db.Open();
+                int result = db.Execute(@"DELETE FROM [dbo].[Tbl_Student]
+WHERE [StudentName] = 'Aung Aung';");
+
+                Console.WriteLine($"Row affected: {result}");
+            }
+        }
+
+    }
+}
---
+++ DotNetPractice.ConsoleApp5/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DotNetPractice.ConsoleApp5;
+
+Console.WriteLine("Hello, World!");
+
+DapperService service = new DapperService();
+//service.Read();
+//service.Create();
+//service.Update();
+service.Delete();
+
+Console.ReadLine();
