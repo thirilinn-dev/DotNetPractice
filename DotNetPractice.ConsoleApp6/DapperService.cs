181835b8dc9666399ff7f3fca55b4b4f01c9fad7
thirilinn-coder <thirilinn.developer@gmail.com>
2026-09-02 23:53:54 +0630

    feat: user crud using dapper

---
+++ DotNetPractice.ConsoleApp6/DapperService.cs
@@ -0,0 +1,86 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetPractice.ConsoleApp6
+{
+
+    internal class DapperService
+    {
+        private readonly SqlConnectionStringBuilder sb = new SqlConnectionStringBuilder()
+        {
+            DataSource = ".",
+            InitialCatalog = "DotNetPractice",
+            UserID = "sa",
+            Password = "sasa@123",
+            TrustServerCertificate = true
+        };
+
+        public void Read()
+        {
+            using(IDbConnection connection = new SqlConnection(sb.ConnectionString))
+            {
+                connection.Open();
+                List<dynamic> lst = connection.Query<dynamic>(@"SELECT [UserId]
+      ,[Username]
+      ,[Password]
+  FROM [dbo].[Tbl_User]").ToList();
+
+                foreach (var item in lst)
+                {
+                    Console.WriteLine($"Id: {item.UserId}, Name: {item.Username}, Password: {item.Password}");
+                }
+            }
+        }
+
+        public void Create()
+        {
+            using(IDbConnection connection = new SqlConnection( sb.ConnectionString))
+            {
+                connection.Open();
+                int result = connection.Execute(@"INSERT INTO [dbo].[Tbl_User]
+           ([Username]
+           ,[Password])
+VALUES
+           ('admin', 'admin123'),
+           ('lynn', 'lynn123'),
+           ('student01', 'student123');");
+
+                Console.WriteLine($"Row affected: {result}");
+            }
+        }
+
+        public void Update()
+        {
+            using(IDbConnection connection = new SqlConnection(sb.ConnectionString))
+            {
+                connection.Open();
+
+                int result = connection.Execute(@"UPDATE [dbo].[Tbl_User]
+SET [Username] = 'student02',
+    [Password] = 'newpass123'
+WHERE [Username] = 'student01';");
+
+                Console.WriteLine("row affected: " + result);
+            }
+        }
+
+        public void Delete()
+        {
+            using( IDbConnection connection = new SqlConnection(sb.ConnectionString) )
+            {
+                connection.Open();
+
+                int result = connection.Execute(@"DELETE FROM [dbo].[Tbl_User]
+WHERE [Username] = 'admin';");
+
+                Console.WriteLine($"row affected: {result}");
+            }
+        }
+    }
+}
---
+++ DotNetPractice.ConsoleApp6/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DotNetPractice.ConsoleApp6;
+
+Console.WriteLine("Hello, World!");
+
+DapperService ds = new DapperService();
+//ds.Read();
+//ds.Create();
+//ds.Update();
+ds.Delete();
+
+Console.ReadLine();
